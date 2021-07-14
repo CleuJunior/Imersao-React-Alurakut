7405cd06afcd8f32323aecd79c84032bbcbdd4c6
Cleonildo Soares Guimaraes Junior <cleonildo.junior@b2wdigital.com>
2021-07-13 22:32:53 -0300

    Criando Elementos

--- alurakut/pages/_app.js
+++ alurakut/pages/_app.js
@@ -1,12 +1,24 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  /* Reset CSS */
+
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+`;
 
 const theme = {
   colors: {
--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,10 +1,40 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+`;
+
+const MainGrid = styled.main`
+  display: grid;
+  grid-gap: 10px;
+  padding: 16px;
+  
+  @media(min-width: 860px){
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 618px 312px;
+  }
+  grid-template-columns: ;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return( 
+
+    <MainGrid>
+      <Box style={{ gridArea: 'profileArea' }}>
+        Imagem
+      </Box>
+      <Box>
+        Bem vindo
+      </Box>
+      <Box>
+        Comunidades
+      </Box>
+    </MainGrid>
+  )
 }
